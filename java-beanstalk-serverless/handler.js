e2666cff2705accdb09b46f0e26b48a27bc2fc7d
Juho Naalisvaara <juho.naalisvaara@gmail.com>
2018-02-20 16:59:18 +0200

    add lambda that needs to know the endpoint of the beanstalked application

--- java-beanstalk-serverless/handler.js
+++ java-beanstalk-serverless/handler.js
@@ -14,3 +14,20 @@ module.exports.helloFromLambda = (event, context, callback) => {
 
   callback(null, response);
 };
+
+module.exports.requiresBeanstalk = (event, context, callback) => {
+
+    var beanStalkUrl = process.env.BEANSTALK_APP;
+
+    console.log('executing requiresBeanstalk ', beanStalkUrl);
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: 'Lambda that knows about beanstalk ' +beanStalkUrl,
+            input: event,
+        }),
+    };
+
+    callback(null, response);
+};
